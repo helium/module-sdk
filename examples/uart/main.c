f88b4f1e4a0fc7786515a75ecb052bf382e1e268
Louis Thiery <louis@helium.com>
2018-11-08 08:33:15 -0800

    cleaned up adc

---
+++ examples/uart/main.c
@@ -0,0 +1,14 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <uart.h>
+#include <timer.h>
+
+int main(void) {
+  printf("[UART] Example started\r\n");
+
+  
+
+  return 0;
+}
--- libtock/adc.c
+++ libtock/adc.c
@@ -16,16 +16,6 @@ enum cmd {
   SINGLE_SAMPLE = 1,
 };
 
-int adc_set_callback(subscribe_cb callback, void* callback_args);
-int adc_set_callback(subscribe_cb callback, void* callback_args) {
-  return subscribe(DRIVER_NUM_ADC, BUFFER_RECEIVE_CB, callback, callback_args);
-}
-
-int adc_single_sample(uint8_t channel);
-int adc_single_sample(uint8_t channel) {
-  return command(DRIVER_NUM_ADC, SINGLE_SAMPLE, channel, 0);
-}
-
 typedef struct adc_req {
   int error;
   uint8_t channel;
@@ -33,44 +23,23 @@ typedef struct adc_req {
   struct adc_req* next;
 } adc_req_t;
 
-static adc_req_t *adc_req_tail = NULL;
+// callback definition given to Tock
+static void adc_cb(int _x __attribute__ ((unused)), int channel, int sample, void* completed_request);
+// internal helpers
+static int adc_set_callback(subscribe_cb callback, adc_req_t* req);
+static int adc_single_sample(uint8_t channel);
+static void dummy_cb(int chan __attribute__ ((unused)), uint16_t sample, bool queue_empty __attribute__((unused)));
+static int adc_dispatch_request(adc_req_t* req);
 
-// Internal callback for creating synchronous functions
-static void adc_cb(int _x __attribute__ ((unused)),
-                   int channel,
-                   int sample,
-                   void* callback_args) {
-
-  adc_req_t* cur_req = (adc_req_t*) callback_args;
-
-  if (cur_req->user_cb == NULL) {
-    // there is a bug in adc.c if this happened
-  }
-
-  adc_read_cb cb  = cur_req->user_cb;
-  adc_req_t* next = cur_req->next;
-  free(cur_req);
-  (*cb)(channel, sample, next == NULL);
-
-  if (next != NULL) {
-    next->error = adc_set_callback(adc_cb, (void*) next);
-    if (next->error < TOCK_SUCCESS) return;
-
-    next->error = adc_single_sample(next->channel);
-    if (next->error < TOCK_SUCCESS) return;
-  }else {
-    adc_req_tail = NULL;
-  }
-}
-
+// dummy async callback for synchronous calls
 static uint16_t * dummy_value;
 static bool dummy_fired;
-void dummy_cb(int chan __attribute__ ((unused)), uint16_t sample, bool queue_empty __attribute__((unused)));
-void dummy_cb(int chan __attribute__ ((unused)), uint16_t sample, bool queue_empty __attribute__((unused))){
+static void dummy_cb(int chan __attribute__ ((unused)), uint16_t sample, bool queue_empty __attribute__((unused))){
   *dummy_value = sample;
   dummy_fired  = true;
 }
 
+// Synchronous read simply exercises async call
 int adc_read(uint8_t channel, uint16_t* sample) {
   dummy_value = sample;
   dummy_fired = false;
@@ -83,32 +52,80 @@ int adc_read(uint8_t channel, uint16_t* sample) {
   return TOCK_SUCCESS;
 }
 
+
+// Request Queue
+static adc_req_t *adc_req_queue = NULL;
+// Asynchronous Call
 int adc_read_async(uint8_t channel, adc_read_cb cb){
-  int err;
   adc_req_t* request = (adc_req_t*)malloc(sizeof(adc_req_t));
   if (request == NULL) return TOCK_ENOMEM;
 
-  request->user_cb = cb;
-  request->next    = NULL;
-
+  request->user_cb   = cb;
+  request->next      = NULL;
+  request->channel   = channel;
   // there is a queue
-  if (adc_req_tail != NULL) {
+  if (adc_req_queue != NULL) {
     // insert self in queue
-    request->channel   = channel;
-    adc_req_tail->next = request;
-    adc_req_tail       = request;
+    adc_req_queue->next = request;
+    adc_req_queue       = request;
+    return TOCK_SUCCESS;
   }
-  // otherwise, just execute now
+  // otherwise, no queue so just execute now
   else{
-    adc_req_tail = request;
-
-    err = adc_set_callback(adc_cb, (void*) request);
-    if (err < TOCK_SUCCESS) return err;
-
-    err = adc_single_sample(channel);
-    if (err < TOCK_SUCCESS) return err;
-
+    adc_req_queue = request;
+    return adc_dispatch_request(request);
   }
+  
+}
+
+static int adc_set_callback(subscribe_cb callback, adc_req_t* req) {
+  return subscribe(DRIVER_NUM_ADC, BUFFER_RECEIVE_CB, callback, (void*) req);
+}
+
+static int adc_single_sample(uint8_t channel) {
+  return command(DRIVER_NUM_ADC, SINGLE_SAMPLE, channel, 0);
+}
+
+static int adc_dispatch_request(adc_req_t* req){
+  int err;
+
+  err = adc_set_callback(adc_cb, req);
+  if (err < TOCK_SUCCESS) return err;
+
+  err = adc_single_sample(req->channel);
+  if (err < TOCK_SUCCESS) return err;
 
   return TOCK_SUCCESS;
 }
+
+// Internal callback
+// Handles internal queue, dispatching new requests
+// And fires user callback
+static void adc_cb(int _x __attribute__ ((unused)),
+                   int channel,
+                   int sample,
+                   void* completed_request) {
+
+  adc_req_t* cur_req = (adc_req_t*) completed_request;
+
+  // keep local copy of callback and next item
+  adc_read_cb cb  = cur_req->user_cb;
+  adc_req_t* next = cur_req->next;
+  // free current request
+  free(cur_req);
+
+  // fire callback (WARNING: we don't check for null pointer because
+  // it's all internal to this library but this is generally unsafe)
+  (*cb)(channel, sample, next == NULL);
+
+  // dispatch next request
+  if (next != NULL) {
+    adc_dispatch_request(next);
+  } else {
+    adc_req_queue = NULL;
+  }
+}
+
+
+
+
