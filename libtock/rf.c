a7f7d54b51e2da2e29f6d5c060e878101ab9e8c1
Robert Keith <robertfkeith@me.com>
2019-03-14 18:32:42 -0700

    update rf to include raw format for sending bytes

--- libtock/rf.c
+++ libtock/rf.c
@@ -22,20 +22,16 @@ enum cmd {
   COMMAND_SET_ADDRESS = 7,
 };
 
-typedef struct rf_req {
-  packet_t* packet;
-  rf_cb user_cb;
-  struct rf_req* next;
-} rf_req_t;
-
 // Request Queue
 static rf_req_t *rf_queue = NULL;
 static bool queue_empty   = false;
 int rf_dispatch_request(rf_req_t* req);
+
 static void tx_done_callback(int result,
                              __attribute__ ((unused)) int arg2,
                              __attribute__ ((unused)) int arg3,
                              void* completed_request) {
+  
   rf_req_t* cur_req = (rf_req_t*) completed_request;
 
   // keep local copy of callback and next item
@@ -45,12 +41,22 @@ static void tx_done_callback(int result,
   // fire callback
 
   if (cb != NULL) {
-    (*cb)(result, cur_req->packet);
-  }
-  // free current request
-  packet_disassemble(cur_req->packet);
-  free(cur_req);
+	if (cur_req->payload->type == PL_TYPE_PACKETIZER) {
+	  (*cb)(result, cur_req->payload);
+	  // free current request
+	  packet_disassemble(cur_req->payload->packet);
+	  free(cur_req->payload);
+	  free(cur_req);
+	} else if (cur_req->payload->type == PL_TYPE_NONE) {
+	  (*cb)(result, cur_req->payload);
+	  free(cur_req->payload->raw_packet->data);
+	  free(cur_req->payload->packet);
+	  free(cur_req->payload);
+	  free(cur_req);
 
+	}
+  }
+  
   // dispatch next request
   if (next != NULL) {
     rf_dispatch_request(next);
@@ -78,20 +84,40 @@ int rf_set_oui(unsigned char *address) {
 
 int rf_dispatch_request(rf_req_t* req){
   int err;
+  switch(req->payload->type){
+	case PL_TYPE_NONE:
+	  err = allow(RF_DRIVER, ALLOW_NUM_W, (void *) req->payload->raw_packet->data, req->payload->raw_packet->len);
+	  if (err < 0) return err;
 
-  err = allow(RF_DRIVER, ALLOW_NUM_W, (void *) req->packet->data, req->packet->len);
-  if (err < 0) return err;
+	  // Subscribe to the transmit callback
+	  err = subscribe(RF_DRIVER, SUBSCRIBE_TX,
+					  tx_done_callback, (void *) req);
+	  if (err < 0) return err;
 
-  // Subscribe to the transmit callback
-  err = subscribe(RF_DRIVER, SUBSCRIBE_TX,
-                  tx_done_callback, (void *) req);
-  if (err < 0) return err;
+	  // Issue the send command and wait for the transmission to be done.
+	  return command(RF_DRIVER, COMMAND_SEND, PL_TYPE_NONE, 0);
+	
+	case PL_TYPE_PACKETIZER:
+	  err = allow(RF_DRIVER, ALLOW_NUM_W, (void *) req->payload->packet->data, req->payload->packet->len);
+	  if (err < 0) return err;
 
-  // Issue the send command and wait for the transmission to be done.
-  return command(RF_DRIVER, COMMAND_SEND, 0, 0);
+	  // Subscribe to the transmit callback
+	  err = subscribe(RF_DRIVER, SUBSCRIBE_TX,
+					  tx_done_callback, (void *) req);
+	  if (err < 0) return err;
+
+	  // Issue the send command and wait for the transmission to be done.
+	  return command(RF_DRIVER, COMMAND_SEND, PL_TYPE_PACKETIZER, 0);
+	case PL_TYPE_CAUTERIZE:
+	  return 0;
+	default:
+	  return 0;
+  }
 }
 
+rf_req_t* allocate_raw_request(raw_packet_t* packet, rf_cb cb);
 rf_req_t* allocate_request(packet_t* packet, rf_cb cb);
+
 rf_req_t* allocate_request(packet_t* packet, rf_cb cb){
   rf_req_t* request = (rf_req_t*) calloc(1,sizeof(rf_req_t));
 
@@ -105,8 +131,28 @@ rf_req_t* allocate_request(packet_t* packet, rf_cb cb){
     if (request == NULL) return NULL;
   }
 
-  request->packet  = packet;
-  request->user_cb = cb;
+  request->payload->type	= PL_TYPE_PACKETIZER;
+  request->payload->packet	= packet;
+  request->user_cb	= cb;
+  return request;
+}
+
+rf_req_t* allocate_raw_request(raw_packet_t* packet, rf_cb cb){
+  rf_req_t* request = (rf_req_t*) calloc(1,sizeof(rf_req_t));
+
+  if (request == NULL) {
+    if (!queue_empty) {
+      yield_for(&queue_empty);
+      request = (rf_req_t*) calloc(1,sizeof(rf_req_t));
+    }else {
+      return NULL;
+    }
+    if (request == NULL) return NULL;
+  }
+  
+  request->payload->type		= PL_TYPE_NONE;
+  request->payload->raw_packet	= packet;
+  request->user_cb		= cb;
   return request;
 }
 
@@ -129,10 +175,27 @@ int rf_send_async(packet_t* packet, rf_cb cb) {
   }
 }
 
+int rf_send_raw_async(raw_packet_t* packet, rf_cb cb) {
+  rf_req_t * request = allocate_raw_request(packet, cb);
+
+  // there is a queue
+  if (rf_queue != NULL) {
+    queue_empty = false;
+    // insert self in queue
+    request->next = request;
+    request       = request;
+    return TOCK_SUCCESS;
+  }
+  // otherwise, no queue so just execute now
+  else{
+    queue_empty = request;
+    return rf_dispatch_request(request);
+  }
+}
 // Request Queue
 static int dummy_result;
 bool fired;
-static void dummy_cb(int result, packet_t* packet __attribute__ ((unused))){
+static void dummy_cb(int result, payload_t* req __attribute__ ((unused))){
   dummy_result = result;
   fired        = true;
 }
@@ -152,4 +215,19 @@ int rf_send(packet_t* packet) {
   return dummy_result;
 }
 
+int rf_send_raw(raw_packet_t* packet) {
+  int err;
+  fired = false;
+  err	= rf_send_raw_async(packet, &dummy_cb);
+  if (err < 0) {
+	free(packet->data);
+	free(packet);
+	free(rf_queue);
+	rf_queue = NULL;
+	return err;
+  }
+
+  yield_for(&fired);
+  return dummy_result;
+}
 
