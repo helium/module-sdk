666b505392c39b6f3fed6a66bc8c0289931e37f9
Robert Keith <robertfkeith@me.com>
2019-03-26 17:50:17 -0700

    added SA demo which is a lot like the ti demo

---
+++ examples/sa-demo/Adafruit_GPS/Adafruit_GPS.c
@@ -0,0 +1,337 @@
+/***********************************
+   This is our GPS library
+
+   Adafruit invests time and resources providing this open source code,
+   please support Adafruit and open-source hardware by purchasing
+   products from Adafruit!
+
+   Written by Limor Fried/Ladyada for Adafruit Industries.
+   BSD license, check license.txt for more information
+   All text above must be included in any redistribution
+ ****************************************/
+#include "Adafruit_GPS.h"
+#include <ctype.h>
+#include <math.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <uart.h>
+
+// how long are max NMEA lines to parse?
+#define MAXLINELENGTH 124
+
+// we double buffer: read one line in and leave one for the main program
+// our index into filling the current line
+// volatile uint8_t lineidx = 0;
+// pointers to the double buffers
+uint8_t buf[MAXLINELENGTH];
+
+bool parseGPS(struct GPS* gps, char *nmea) {
+  // do checksum check
+  // first look if we even have one
+  if (nmea[strlen(nmea) - 4] == '*') {
+    uint16_t sum = parseHexGPS(nmea[strlen(nmea) - 3]) * 16;
+    sum += parseHexGPS(nmea[strlen(nmea) - 2]);
+
+    // check checksum
+    for (uint8_t i = 2; i < (strlen(nmea) - 4); i++) {
+      sum ^= nmea[i];
+    }
+    if (sum != 0) {
+      // bad checksum :(
+      return false;
+    }
+  }
+  int32_t degree;
+  long minutes;
+  char degreebuff[10];
+  // look for a few common sentences
+  if (strstr(nmea, "$GPGGA")) {
+    // found GGA
+    char *p = nmea;
+    // get time
+    p = strchr(p, ',') + 1;
+    float timef   = atof(p);
+    uint32_t time = timef;
+    gps->hour    = time / 10000;
+    gps->minute  = (time % 10000) / 100;
+    gps->seconds = (time % 100);
+
+    gps->milliseconds = fmod(timef, 1.0) * 1000;
+
+    // parse out latitude
+    p = strchr(p, ',') + 1;
+    if (',' != *p) {
+      strncpy(degreebuff, p, 2);
+      p += 2;
+      degreebuff[2] = '\0';
+      degree        = atol(degreebuff) * 10000000;
+      strncpy(degreebuff, p, 2); // minutes
+      p += 3; // skip decimal point
+      strncpy(degreebuff + 2, p, 4);
+      degreebuff[6]         = '\0';
+      minutes               = 50 * atol(degreebuff) / 3;
+      gps->latitude_fixed   = degree + minutes;
+      gps->lat_degree       = degree;
+      gps->lat_minutes      = minutes;
+      gps->latitude         = degree / 100000 + minutes * 0.000006F;
+      gps->latitudeDegrees  = (gps->latitude - 100 * (int)(gps->latitude / 100)) / 60.0;
+      gps->latitudeDegrees += (int)(gps->latitude / 100);
+    }
+
+    p = strchr(p, ',') + 1;
+    if (',' != *p) {
+      if (p[0] == 'S') gps->latitudeDegrees *= -1.0;
+      if (p[0] == 'N') gps->lat = 'N';
+      else if (p[0] == 'S') gps->lat = 'S';
+      else if (p[0] == ',') gps->lat = 0;
+      else return false;
+    }
+
+    // parse out longitude
+    p = strchr(p, ',') + 1;
+    if (',' != *p) {
+      strncpy(degreebuff, p, 3);
+      p += 3;
+      degreebuff[3] = '\0';
+      degree        = atol(degreebuff) * 10000000;
+      strncpy(degreebuff, p, 2); // minutes
+      p += 3; // skip decimal point
+      strncpy(degreebuff + 2, p, 4);
+      degreebuff[6] = '\0';
+      minutes       = 50 * atol(degreebuff) / 3;
+      gps->longitude_fixed   = degree + minutes;
+      gps->lon_degree           = degree;
+      gps->lon_minutes          = minutes;
+      gps->longitude         = degree / 100000 + minutes * 0.000006F;
+      gps->longitudeDegrees  = (gps->longitude - 100 * (int)(gps->longitude / 100)) / 60.0;
+      gps->longitudeDegrees += (int)(gps->longitude / 100);
+    }
+
+    p = strchr(p, ',') + 1;
+    if (',' != *p) {
+      if (p[0] == 'W') gps->longitudeDegrees *= -1.0;
+      if (p[0] == 'W') gps->lon = 'W';
+      else if (p[0] == 'E') gps->lon = 'E';
+      else if (p[0] == ',') gps->lon = 0;
+      else return false;
+    }
+
+    p = strchr(p, ',') + 1;
+    if (',' != *p) {
+      gps->fixquality = atoi(p);
+    }
+
+    p = strchr(p, ',') + 1;
+    if (',' != *p) {
+      gps->satellites = atoi(p);
+    }
+
+    p = strchr(p, ',') + 1;
+    if (',' != *p) {
+      gps->HDOP = atof(p);
+    }
+
+    p = strchr(p, ',') + 1;
+    if (',' != *p) {
+      gps->altitude = atof(p);
+    }
+
+    p = strchr(p, ',') + 1;
+    p = strchr(p, ',') + 1;
+    if (',' != *p) {
+      gps->geoidheight = atof(p);
+    }
+    return true;
+  }
+  if (strstr(nmea, "$GPRMC")) {
+    // found RMC
+    char *p = nmea;
+
+    // get time
+    p = strchr(p, ',') + 1;
+    float timef   = atof(p);
+    uint32_t time = timef;
+    gps->hour    = time / 10000;
+    gps->minute  = (time % 10000) / 100;
+    gps->seconds = (time % 100);
+
+    gps->milliseconds = fmod(timef, 1.0) * 1000;
+
+    p = strchr(p, ',') + 1;
+    if (p[0] == 'A')
+      gps->fix = true;
+    else if (p[0] == 'V')
+      gps->fix = false;
+    else
+      return false;
+
+    // parse out latitude
+    p = strchr(p, ',') + 1;
+    if (',' != *p) {
+      strncpy(degreebuff, p, 2);
+      p += 2;
+      degreebuff[2] = '\0';
+      degree        = atol(degreebuff) * 10000000;
+      strncpy(degreebuff, p, 2); // minutes
+      p += 3; // skip decimal point
+      strncpy(degreebuff + 2, p, 4);
+      degreebuff[6]         = '\0';
+      minutes               = 50 * atol(degreebuff) / 3;
+      gps->latitude_fixed   = degree + minutes;
+      gps->lat_degree           = degree;
+      gps->lat_minutes          = minutes;
+      gps->latitude         = degree / 100000 + minutes * 0.000006F;
+      gps->latitudeDegrees  = (gps->latitude - 100 * (int)(gps->latitude / 100)) / 60.0;
+      gps->latitudeDegrees += (int)(gps->latitude / 100);
+    }
+
+    p = strchr(p, ',') + 1;
+    if (',' != *p) {
+      if (p[0] == 'S') gps->latitudeDegrees *= -1.0;
+      if (p[0] == 'N') gps->lat = 'N';
+      else if (p[0] == 'S') gps->lat = 'S';
+      else if (p[0] == ',') gps->lat = 0;
+      else return false;
+    }
+
+    // parse out longitude
+    p = strchr(p, ',') + 1;
+    if (',' != *p) {
+      strncpy(degreebuff, p, 3);
+      p += 3;
+      degreebuff[3] = '\0';
+      degree        = atol(degreebuff) * 10000000;
+      strncpy(degreebuff, p, 2); // minutes
+      p += 3; // skip decimal point
+      strncpy(degreebuff + 2, p, 4);
+      degreebuff[6] = '\0';
+      minutes       = 50 * atol(degreebuff) / 3;
+      gps->longitude_fixed   = degree + minutes;
+      gps->lon_degree           = degree;
+      gps->lon_minutes          = minutes;
+      gps->longitude         = degree / 100000 + minutes * 0.000006F;
+      gps->longitudeDegrees  = (gps->longitude - 100 * (int)(gps->longitude / 100)) / 60.0;
+      gps->longitudeDegrees += (int)(gps->longitude / 100);
+    }
+
+    p = strchr(p, ',') + 1;
+    if (',' != *p) {
+      if (p[0] == 'W') gps->longitudeDegrees *= -1.0;
+      if (p[0] == 'W') gps->lon = 'W';
+      else if (p[0] == 'E') gps->lon = 'E';
+      else if (p[0] == ',') gps->lon = 0;
+      else return false;
+    }
+    // speed
+    p = strchr(p, ',') + 1;
+    if (',' != *p) {
+      gps->speed = atof(p);
+    }
+
+    // angle
+    p = strchr(p, ',') + 1;
+    if (',' != *p) {
+      gps->angle = atof(p);
+    }
+
+    p = strchr(p, ',') + 1;
+    if (',' != *p) {
+      uint32_t fulldate = atof(p);
+      gps->day   = fulldate / 10000;
+      gps->month = (fulldate % 10000) / 100;
+      gps->year  = (fulldate % 100);
+    }
+    // we dont parse the remaining, yet!
+    return true;
+  }
+
+  return false;
+}
+
+// Initialization code used by all constructor types
+void GPS_init(struct GPS* gps) {
+  gps->recvdflag   = false;
+  gps->paused      = false;
+  gps->lineidx     = 0;
+  gps->hour = gps->minute = gps->seconds = gps->year = gps->month = gps->day =
+                                                                      gps->fixquality = gps->satellites = 0; // uint8_t
+  gps->lat = gps->lon = gps->mag = 0; // char
+  gps->fix = false; // boolean
+  gps->milliseconds = 0; // uint16_t
+  gps->latitude     = gps->longitude = gps->geoidheight = gps->altitude =
+                                                            gps->speed = gps->angle = gps->magvariation = gps->HDOP =
+                                                                                                            0.0; // float
+}
+
+
+
+void pauseGPS(struct GPS* gps, bool p) {
+  gps->paused = p;
+}
+
+char *lastNMEA(struct GPS* gps) {
+  gps->recvdflag = false;
+  printf("%s \r\n", gps->lastline);
+  return (char *)gps->lastline;
+}
+
+// read a Hex value and return the decimal equivalent
+uint8_t parseHexGPS(char c) {
+  if (c < '0')
+    return 0;
+  if (c <= '9')
+    return c - '0';
+  if (c < 'A')
+    return 0;
+  if (c <= 'F')
+    return (c - 'A') + 10;
+  // if (c > 'F')
+  return 0;
+}
+
+bool waitForSentence(struct GPS* gps, const char *wait4me) {
+  uint8_t max = MAXWAITSENTENCE;
+  char str[20];
+
+  uint8_t i = 0;
+  while (i < max) {
+    readGPS(gps);
+
+    if (gps->recvdflag) {
+      gps->recvdflag = false;
+      char* nmea = gps->lastline;
+
+      strncpy(str, nmea, 20);
+      str[19] = 0;
+      i++;
+      if (strstr(str, wait4me)) return true;
+    }
+  }
+
+  return false;
+}
+
+
+// Standby Mode Switches
+bool standbyGPS(struct GPS* gps) {
+  if (gps->inStandbyMode) {
+    return false;  // Returns false if already in standby mode, so that you do not wake it up by sending commands to GPS
+  }else {
+    gps->inStandbyMode = true;
+    sendCommand(gps, PMTK_STANDBY);
+    // return waitForSentence(PMTK_STANDBY_SUCCESS);  // don't seem to be fast enough to catch the message, or something else just is not working
+    return true;
+  }
+}
+
+bool wakeupGPS(struct GPS* gps) {
+  if (gps->inStandbyMode) {
+    gps->inStandbyMode = false;
+    sendCommand(gps, "");  // send byte to wake it up
+    return waitForSentence(gps, PMTK_AWAKE);
+  }else {
+    return false;    // Returns false if not in standby mode, nothing to wakeup
+  }
+}
---
+++ examples/sa-demo/main.c
@@ -0,0 +1,164 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "Adafruit_GPS/Adafruit_GPS.h"
+#include "bosch_port/bosch_port.h"
+#include "opt3001/opt3001.h"
+#include "bme280/bme280_port.h"
+
+#include <i2c_master.h>
+#include <led.h>
+#include <led.h>
+#include <rf.h>
+#include <timer.h>
+#include <uart.h>
+
+static bool event = false;
+static bool gps_read_fired = false;
+static bool timer_fired = false;
+
+struct opt3001_dev opt3001;
+struct bme280_dev bme280;
+struct bme280_data bme_data;
+
+int8_t bme280_setup(struct bme280_dev *dev);
+int8_t bme280_get_sample_forced_mode(struct bme280_dev *dev, struct bme280_data *data);
+int8_t opt3001_get_lux(struct opt3001_dev *dev);
+
+void gps_read_cb(void
+  );
+void gps_read_cb(void){
+  gps_read_fired = true;
+  event = true;
+}
+
+static void timer_callback( __attribute__ ((unused)) int arg0,
+                            __attribute__ ((unused)) int arg1,
+                            __attribute__ ((unused)) int arg2,
+                            __attribute__ ((unused)) void *ud) {
+  timer_fired = true;
+  event = true;
+}
+
+// Timer callback
+tock_timer_t simple_timer;
+
+int main(void) {
+  printf("[SA Demo]\r\n");
+
+  if (!OPT3001_init(&opt3001, NULL)) {
+     printf_async("OPT3001 Initialization Failed\r\n");
+    while (1) ;
+  }else {
+    printf_async("OPT3001 Initialized\r\n");
+  }
+
+  if (!rf_driver_check()) {
+    printf("Driver check OK\r\n");
+  } else {
+    printf("Driver check FAIL\r\n");
+  }
+
+  if (!rf_enable()) {
+    printf("Radio init OK\r\n");
+  } else {
+    printf("Radio init FAIL\r\n");
+  }
+
+  uint8_t id = 0xFF;
+  uint16_t seq = 0;
+
+  timer_every(1000, timer_callback, NULL, &simple_timer);
+
+  while(1){
+    yield_for(&event);
+    event = false;
+
+    if(timer_fired){
+      int result;
+      
+      // Read the Atmospheric sensor
+      result = bme280_get_sample_forced_mode(&bme280, &bme_data);
+        if (result) {
+        printf_async("BME280 Fail");
+      }else {
+        printf_async("BME280 OK");
+      }
+
+      // Read the optical sensor
+      float lux_data;
+      result = !OPT3001_getLux(&opt3001, &lux_data);
+      if (result) {
+        printf_async("OPT3001 Lux fail");
+      }else {
+        printf_async("LUX data OK");
+      }
+
+      timer_fired = false;
+      raw_packet_t pkt;
+      uint8_t buffer[14];
+      pkt.len = 14;
+      pkt.data = &buffer;
+
+      uint32_t tmp = &bme_data.temperature;
+      uint32_t pres = &bme_data.pressure;
+      uint8_t lux = (int) lux_data;
+      int16_t hum = (uint16_t)&bme_data.humidity;
+      uint byte_counter = 0;
+      memcpy(buffer, (void*)&id, sizeof(uint8_t));
+      byte_counter += sizeof(uint8_t);
+      memcpy(buffer + byte_counter, (void*)&seq, sizeof(uint16_t));
+      byte_counter += sizeof(uint16_t);
+      memcpy(buffer + byte_counter, (void*)&tmp, sizeof(float));
+      byte_counter += sizeof(float);
+      memcpy(buffer + byte_counter, (void*)&pres, sizeof(float));
+      byte_counter += sizeof(float);
+      memcpy(buffer + byte_counter, &lux_data, sizeof(uint8_t)); 
+      byte_counter += sizeof(uint8_t);
+      memcpy(buffer + byte_counter, &hum, sizeof(int16_t));
+      rf_send_raw(&pkt);
+      printf_async("\r\n");
+      seq++;
+     
+    }
+  };
+ 
+  return 0;
+}
+
+int8_t bme280_setup(struct bme280_dev *dev)
+{
+  uint8_t settings_sel;
+
+  /* Recommended mode of operation: Indoor navigation */
+  dev->settings.osr_h  = BME280_OVERSAMPLING_1X;
+  dev->settings.osr_p  = BME280_OVERSAMPLING_16X;
+  dev->settings.osr_t  = BME280_OVERSAMPLING_2X;
+  dev->settings.filter = BME280_FILTER_COEFF_16;
+
+  settings_sel = BME280_OSR_PRESS_SEL | BME280_OSR_TEMP_SEL | BME280_OSR_HUM_SEL | BME280_FILTER_SEL;
+
+  return bme280_set_sensor_settings(settings_sel, dev);
+}
+
+int8_t bme280_get_sample_forced_mode(struct bme280_dev *dev, struct bme280_data *comp_data)
+{
+  // struct bme280_data comp_data;
+  uint8_t rslt = bme280_set_sensor_mode(BME280_FORCED_MODE, dev);
+
+  if (rslt != BME280_OK) {
+    return rslt;
+  }
+
+  /* Wait for the measurement to complete and print data @25Hz */
+  dev->delay_ms(40);
+
+  rslt = bme280_get_sensor_data(BME280_ALL, comp_data, dev);
+  if (rslt != BME280_OK) {
+    return rslt;
+  }
+  return rslt;
+}
+
